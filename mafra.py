5e98baa4a1179690268fa040b2321a27bd18993c
gonzalo <gonzalomuri@gmail.com>
2019-06-08 19:46:47 +0200

    progress

--- mafra.py
+++ mafra.py
@@ -1,57 +1,88 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import os
 import glob
 import gzip
 import re
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/second_step")
 def second_step():
+    # This is the path that comes from the form
+    ddfs_path = request.args.get('path')
+    # This normalizes the path based on the Operating System
+    ddfs_path = os.path.normcase(ddfs_path)
+    files_and_dates = {}  # Dictionary with the files and dates
+    files_and_dates_list = []
 
-
-    ddfs_path=request.args.get('path') # This is the path that comes from the form
-    ddfs_path=os.path.normcase(ddfs_path) # This normalizes the path based on the Operating System
-
-    if (ddfs_path == ''): # if it is empty, we return error
-        return (render_template('empty_path.html')) # Path was empty
+    if (ddfs_path == ''):  # if it is empty, we return error
+        return (render_template('empty_path.html'))  # Path was empty
 
     else:
-        ddfs_files_path=ddfs_path+"/ddfs*" # on the specified path we want to search for all ddfs files
-        ddfs_files=[]
+        # on the specified path we want to search for all ddfs files
+        ddfs_files_path = ddfs_path+"/ddfs*"
+        ddfs_files = []
+        files_and_dates_list=[]
+        found=False # If we find a
+
         for file in glob.glob(ddfs_files_path):
-            ddfs_files.append(file) # a list with all the ddfs files present in the path
-        files_and_dates={} # Dictionary with the files and dates
-        regexp=r'[0-9][0-9]/[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]'
+            # a list with all the ddfs files present in the path
+            ddfs_files.append(file)
 
         # Now we need to open every file to obtain its generated date
         for index,f in enumerate(ddfs_files):
+            # The could be two types of ddfs files
+            # normal files and compressed files .gz
+            # We need to detect both of them and proceed
 
-            if ".gz" in f:
-                with gzip.open(f, "rt") as file:
-                    for line in file:
-                        matchObj=re.match(regexp, line)
-                        if matchObj:
-                            files_and_dates[os.path.basename(ddfs_files[index])]=matchObj.group() # Add to the dictionary the name of the file and line
-                            break
+            found=False # For each file we set found = False
+            if ".gz" in f:  # gzipped version of ddfs file
+                opener = gzip.open
             else:
-                with open(f) as file:
-                    for line in file:
-                        matchObj = re.match(regexp, line)
-                        if matchObj:
-                            files_and_dates[os.path.basename(ddfs_files[
-                                                                 index])] = matchObj.group()  # Add to the dictionary the name of the file and line
-                            break
+                opener = open
 
-    print(files_and_dates)
-    return render_template('second_step.html',path=ddfs_path,ddfs_files=ddfs_files,files_and_dates=files_and_dates)
+            with opener(f, "rt") as file:
+
+                for line in file:
+
+                    # Regular expression for catching the time in the ddfs log
+                    regexp = r'[0-9][0-9]/[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]'
+
+                    matchObj = re.match(regexp, line)  # Does it match what we search at the start of the line?
+                    if matchObj:
+                        # Add to the dictionary the name of the file and line
+                        files_and_dates['checkbox'] = ""
+                        files_and_dates['name_of_file'] = os.path.basename(ddfs_files[index])
+                        files_and_dates['start_date'] = matchObj.group()
+
+                        files_and_dates_list.append(files_and_dates)
+                        files_and_dates={} #  new reference in memory
+                        found=True
+                        break
+                # If after checking all the files, we have not found a line that matches the date format
+                # then we do know the date
+                if(not found):
+                    files_and_dates['checkbox'] = ""
+                    files_and_dates['name_of_file'] = os.path.basename(ddfs_files[index])
+                    files_and_dates['start_date'] = "UNKNOWN"
+
+                    files_and_dates_list.append(files_and_dates)
+                    files_and_dates = {}  # new reference in memory
+
+
+    return render_template('second_step.html',path = ddfs_path,ddfs_files = ddfs_files,files_and_dates = files_and_dates,files_and_dates_list=files_and_dates_list)
+
+@app.route("/third_step")
+def third_step():
+    return(render_template("third_step.html"))
 
 if(__name__== "__main__"):
     app.run(debug=True)

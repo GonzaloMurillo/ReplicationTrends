83a2a601f7887249b3d4748b04011b701ecedd2a
gonzalo <gonzalomuri@gmail.com>
2019-07-27 18:10:14 +0200

    Removing prints

--- auxiliar/logparser.py
+++ auxiliar/logparser.py
@@ -42,13 +42,13 @@ class LogParser():
         contexts=[]
         contexts_by_columns=[]
         data_to_plot=[]
-        print("Received {}".format(replication_data))
+        #print("Received {}".format(replication_data))
         for i in range(5,len(replication_data)-2):# Real info of the contexts start in line 5 and is 2 lines less that what we read (removing --- and cumulative from the end)
             contexts.append(replication_data[i])
-        print("Contexts now {}".format(contexts))
+        #print("Contexts now {}".format(contexts))
         for i in range(0,len(contexts)):
             contexts_by_columns.append(contexts[i].split()) # We split by space to have columns
-        print("contexts_by_columns {}".format(contexts_by_columns))
+        #print("contexts_by_columns {}".format(contexts_by_columns))
         for k in range(0,len(contexts_by_columns)):
             # A context with all the fields has 13 columns (starting from 0)
             # However, if it is initializing has 8
@@ -56,27 +56,27 @@ class LogParser():
             if(len(contexts_by_columns[k])==12 or len(contexts_by_columns[k])==9 ):
                 contexts_by_columns[k]=contexts_by_columns[k][2:] # This is to remove two fields that go into the first context and are not required (date and time)
             # Removing from the context_by_column the latest fields, that are just date information of when was in sync
-            print("Before removing {}".format(contexts_by_columns[k]))
+            #print("Before removing {}".format(contexts_by_columns[k]))
             # here we are putting in the same line
             # a field like Thu Jun 20 11:21
             # but it could be that there is no date in contexts_by_columns[k][6] but (initializing)
             # so we need to check
             if("initializing" not in contexts_by_columns[k][6]):
                 contexts_by_columns[k][6]=contexts_by_columns[k][6]+" "+contexts_by_columns[k][7]+" "+contexts_by_columns[k][8]+" "+contexts_by_columns[k][9]
-                print("Despues del invento {}".format(contexts_by_columns[k]))
+                #print("Despues del invento {}".format(contexts_by_columns[k]))
                 for i in range(0,3): # We delete the last 3 items
                     contexts_by_columns[k].pop()
-                    print("Despues de borrar por detras {}".format(contexts_by_columns[k]))
+                    #print("Despues de borrar por detras {}".format(contexts_by_columns[k]))
             
             #contexts_by_columns[k]=contexts_by_columns[k][0:5]
             if(contexts_by_columns[k][0]!='(sum)'): # We do not want to add the cummulative infor
                 data_to_plot.append(contexts_by_columns[k])
                 
-        print("Plotting the following data:\n")
-        print("Size of the list:")
-        print(len(data_to_plot))   
-        print("Actual data to plot:\n")
-        print(data_to_plot)	
+        #print("Plotting the following data:\n")
+        #print("Size of the list:")
+        #print(len(data_to_plot))   
+        #print("Actual data to plot:\n")
+        #print(data_to_plot)	
         return data_to_plot
 
     
